7f575e20edd3fd5cfaa27e954405e2835ef0e753
Nicolas Tudela <nicolastudela@gmail.com>
2019-03-27 17:54:00 -0300

    lesson-1 done

--- src/server.js
+++ src/server.js
@@ -12,4 +12,17 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'HI!!' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'okay' })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
